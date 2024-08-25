b49b935dd4f701a4c5465426607fc5ff69056679
angelitogar <angelito.gar@ciencias.unam.mx>
2024-08-25 12:57:35 -0600

    8/8 programas

--- Practica01ED.hs
+++ Practica01ED.hs
@@ -7,9 +7,27 @@ triangulo b h = sqrt (((b)^2 + (h)^2))
 pendiente :: (Float, Float) -> (Float, Float) -> Float
 pendiente (x1 , y1) (x2 , y2) = (y2 - y1)/ (x2 - x1)
 
-raices :: (Float) -> (Float) -> (Float) -> Float
-raices a b c = ((-2)sqrt (((b)^2-(4(a*c)))/2*a)) 
+raices :: (Float) -> (Float) -> (Float) -> (Float, Float)
+raices a b c = ((((-b+(sqrt(b^2-(4*a*c))))/2*a)), (((-b-(sqrt(b^2-(4*a*c))))/2*a)))
 
 
 areaTriangulo :: Float -> Float -> Float -> Float
 areaTriangulo x y z = sqrt(((x+y+z)/2)*(((x+y+z)/2)-x)*(((x+y+z)/2)-y)*(((x+y+z)/2)-z))
+
+comparador :: Int -> Int -> Int
+comparador x y = if x == y 
+then 0
+else if x < y 
+then -1
+else 1
+
+maximo :: Int -> Int -> Int -> Int
+maximo x y z =  if x > y && x > z
+then x
+else if y > x && y > z
+then y
+else z
+
+esDescendente :: Int -> Int -> Int -> Int -> Bool
+esDescendente x y z w = if x > y && y > z && z > w then True 
+else  False
