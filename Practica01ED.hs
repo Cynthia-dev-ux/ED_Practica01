30b820c7d6a284c16a05a10763000ba6a54b6c04
Cynthia-dev-ux <cynthuau@ciencias.unam.mx>
2024-08-25 16:47:22 -0600

    Practica01ED.hs

--- Practica01ED.hs
+++ Practica01ED.hs
@@ -1,7 +1,7 @@
 distanciaPuntos :: (Float, Float) -> (Float, Float) -> Float
 distanciaPuntos (x1 , y1) (x2, y2) = sqrt(((x2-x1)^2 + (y2-y1)^2))
 
-hipotenusa:: (Float) -> (Float) ->Float
+hipotenusa::(Float) -> (Float) -> (Float)
 hipotenusa b h = sqrt (((b)^2 + (h)^2)) 
 
 pendiente :: (Float, Float) -> (Float, Float) -> Float
